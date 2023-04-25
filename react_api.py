cc193a6126d061370d94740291f8de1ac4a93569
rchandu985 <rchandu985@gmail.com>
2023-04-25 21:32:01 +0530

    added error handling and updated querys

--- react_api.py
+++ react_api.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 from mongoengine import EmailField 
 from mongoengine import connect
+from mongoengine.errors import NotUniqueError
+from mongoengine.errors import ValidationError
 from flask import app
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -16,7 +18,7 @@ db.init_app(app)
 class User(db.Document):
     
     meta = {'collection': 'admin'}
-    username = db.StringField()
+    _id = db.StringField()
     password = db.StringField()
     email=EmailField()
     '''def to_json(self):
@@ -28,8 +30,8 @@ class User(db.Document):
 def query_records():
     data = request.get_json()
     
-    user = User.objects(username=data['username'])
-    print(data)
+    user = User.objects(_id=data['username'])
+    #print(data)
     if not user:
         return jsonify({'error': 'data not found'})
     else:
@@ -39,7 +41,7 @@ def query_records():
 def create_record():
     data = request.get_json()
     
-    user = User.objects(username=data['username']).update(password=data['password'])
+    user = User.objects(_id=data['username']).update(password=data['password'])
     
     if not user:
         return jsonify({'error': 'data not found'})
@@ -50,16 +52,29 @@ def create_record():
 def update_record():
     data = json.loads(request.data)
     
-    user = User.objects.create(username=data['username'],password=data['password'],email=data['email'])
-    return jsonify(data)
+    try:
+        get_data=User.objects(email=data['email'])
+        #print(get_data.first())
+        if get_data.first() is not None:
+        
+            return {"Status":"Entered Email All Ready Exists"}
+        
+        else:
+            user = User.objects.create(_id=data['username'],password=data['password'],email=data['email'])
 
+            return {"Status":"User Created Succesfully"}
+        
+    except NotUniqueError:
+        return {"Status":"Entered Username All ready Exists"}
+    except ValidationError:
+        return {"Status":"Enter The Valid Email Id"}
 @app.route('/', methods=['DELETE'])
 def delete_record():
     data=request.get_json()
     
-    d=User.objects(username=data['username']).first()
+    d=User.objects(_id=data['username']).first()
     d.delete()
-    return {"status":"succesfull_deleted"}
+    return {"status":"succesfully_deleted"}
     
 
 if __name__ == "__main__":
